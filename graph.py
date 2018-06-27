15b2a3b3a4014fb62af015372feb8f006294a44d
Evan Ruderman <evanrman@comcast.net>
2018-06-27 16:13:26 -0400

    Changed plot to show both graphs (stupid mistake)

--- graph.py
+++ graph.py
@@ -8,6 +8,6 @@ plt.title("Temperature Anomalies over Land and over Ocean")
 plt.xlabel("Years")
 plt.ylabel("Temperatures (Degrees Celsius)")
 plt.plot(data["Year"],data["Land_Annual"], '-ro', linewidth = 1, markersize = 3)
-plt.plot(data["Year"],data["Land_Annual"], '-bo', linewidth = 1, markersize = 3)
+plt.plot(data["Year"],data["Ocean_Annual"], '-bo', linewidth = 1, markersize = 3)
 plt.show()
 
