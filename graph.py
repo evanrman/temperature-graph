a912ad4eb629f4c69d7127e928ca247745d33ce3
Evan Ruderman <evanrman@comcast.net>
2018-06-27 16:07:23 -0400

    Extracted data from .csv file and plotted basic graph

---
+++ graph.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("data.csv", sep = ",", skiprows = 1)
+
+plt.title("Temperature Anomalies over Land and over Ocean")
+plt.xlabel("Years")
+plt.ylabel("Temperatures (Degrees Celsius)")
+plt.plot(data["Year"],data["Land_Annual"], '-ro', linewidth = 1, markersize = 3)
+plt.plot(data["Year"],data["Land_Annual"], '-bo', linewidth = 1, markersize = 3)
+plt.show()
+
